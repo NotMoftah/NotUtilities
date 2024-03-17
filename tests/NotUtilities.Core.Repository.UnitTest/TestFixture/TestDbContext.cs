1a0f4c6a3bddf19a13e5c0e759214bd410253b8e
AbdElAziz Moftah <abdelazizimoftah@gmail.com>
2024-03-17 13:13:54 +0100

    added a text fixture base

---
+++ tests/NotUtilities.Core.Repository.UnitTest/TestFixture/TestDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotUtilities.Core.Repository.UnitTest.TestFixture
+{
+    public class TestDbContext : DbContext
+    {
+        public DbSet<TestEntity> TestEntities { get; set; }
+
+        public TestDbContext(DbContextOptions<TestDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TestEntity>().HasKey(e => e.Id);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ tests/NotUtilities.Core.Repository.UnitTest/TestFixture/TestEntity.cs
@@ -0,0 +1,20 @@
+﻿using NotUtilities.Core.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotUtilities.Core.Repository.UnitTest.TestFixture
+{
+    public class TestEntity : IEntity<int>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public TestEntity()
+        {
+            Name = $"name-{Guid.NewGuid()}";
+        }
+    }
+}
